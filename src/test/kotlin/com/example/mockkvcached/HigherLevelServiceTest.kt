6b9895384832ccb00f3a31f38bfbbffff990bf94
Mykola Gurov <ngurov@gmail.com>
2022-01-01 17:15:04 +0100

    a simple spring integration test green

--- src/test/kotlin/com/example/mockkvcached/HigherLevelServiceTest.kt
+++ src/test/kotlin/com/example/mockkvcached/HigherLevelServiceTest.kt
@@ -2,11 +2,26 @@ package com.example.mockkvcached
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 
 class HigherLevelServiceHappyTest {
     @Test
     fun `happy times`() {
         val higherLevelService = HigherLevelService(LowLevelService())
+        //when
+        val actual = higherLevelService.performHigher("blah")
+        //then
+        assertThat(actual).isEqualTo("input(blah_1) - input(blah_2)")
+    }
+}
+
+@SpringBootTest
+class HigherLevelServiceHappyIT(
+    @Autowired val higherLevelService: HigherLevelService
+) {
+    @Test
+    fun `happy times`() {
         //when
         val actual = higherLevelService.performHigher("blah")
         //then
